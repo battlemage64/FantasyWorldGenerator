22e034a049fc36f1e0c4200359ee59b22723f1d1
battlemage64 <arden@kolodner.com>
2018-03-19 21:18:58 -0400

    Added a bunch to lists but didn't actually work on the code. Productive or not? Your call. Considering how tired I am, it's probably good I'm not editing code. That can break much more easily. Anyway, goodnight.

--- lists.py
+++ lists.py
@@ -18,35 +18,35 @@ BIOMETYPES = ('deciduous forest', 'evergreen forest', 'desert',
               'hills')
 
 ANIMALS = {"land_prey_large":("boar", "deer", "buffalo", "giraffe", "hippo", "bison"),
-           "land_prey_small":("rabbit"),
-           "land_predator":("lion", "tiger", "leopard", "hyena"),
+           "land_prey_small":("rabbit", "squirrel"),
+           "land_predator":("lion", "tiger", "leopard", "hyena", "jaguar", "cougar"),
            "bird_predator":("owl", "eagle", "hawk", "vulture", "falcon"),
-           "bird_prey":("sparrow", "finch", "hummingbird", "crow", "raven", "blue jay", "cardinal"),
-           "fish":("trout", "salmon", "flounder", "bass", "tilapia"),
-           "sea":("shark", "octopus", "squid", "shrimp")}
+           "bird_prey":("sparrow", "finch", "hummingbird", "crow", "raven", "blue jay", "cardinal", "duck", "goose", "swallow", "blackbird", "robin"),
+           "fish":("trout", "salmon", "flounder", "bass", "tilapia", "carp", "catfish", "mackerel", "mahi-mahi"),
+           "sea":("shark", "octopus", "squid", "shrimp", "sea snail", "plankton")}
 
-PLANTS = {"vegetables":("onion", "lettuce", "kale", "broccoli"),
-          "fruits":("strawberry", "raspberry", "orange", "lemon", "grapefruit", "grape")}
+PLANTS = {"vegetables":("onion", "lettuce", "kale", "broccoli", "spinach", "turnip", "celery"),
+          "fruits":("strawberry", "raspberry", "orange", "lemon", "grapefruit", "grape", "blackberry", "blueberry", "mango", "lychee")}
 
 REASONS_TO_LEAVE = {"hunt&gath":("following the {0} herds".format(random.choice(ANIMALS["land_prey_large"])),
-                                 "crossing a land bridge", "looking for a better hunting ground", "crossing a wide river", "exploring out of curiosity"),
+                                 "crossing a land bridge", "looking for a better hunting ground", "crossing a wide river", "exploring out of curiosity", "fleeing pursuers"),
                     "agriculture":("seeking better farmland", "looking for a new place to settle")}
 
 ROLES = {"hunt&gath":("hunter", "gatherer", "healer", "shaman", "forager", "youth")}
 
 GROUPS = {"hunt&gath":("tribe", "clan", "band", "pack")}
 
-NOUNS = {"singular":("bird", "bee", "bug", "animal", "food", "club", "spice", "sword", "rock", "sand", "dirt", "coal", "sky", "leaf", "pebble", "plant", "rain", "lightning", "light", "thunder"),
-                    "plural":("birds", "bees", "bugs", "animals", "foods", "clubs", "spices", "swords", "rocks", "sands", "dirts", "coals", "skies", "leaf", "pebble", "plant", "rains", "lights")}
+NOUNS = {"singular":("bird", "bee", "bug", "animal", "food", "club", "spice", "sword", "rock", "sand", "dirt", "coal", "sky", "leaf", "pebble", "plant", "rain", "lightning", "light", "thunder", "fish", "child"),
+                    "plural":("birds", "bees", "bugs", "animals", "foods", "clubs", "spices", "swords", "rocks", "sands", "dirts", "coals", "skies", "leaf", "pebble", "plant", "rains", "lights", "fish", "children")}
 
-VERBS = {"1stsin":("call", "run", "am", "was", "want", "eat", "drink", "die", "read", "play", "fall", "slap", "skip", "walk", "sing", "fight", "bash", "lead", "fall", "strike", "roar"),
-         "2ndsin/3rdpl":("call", "run", "are", "were", "want", "eat", "drink", "die", "read", "play", "fall", "slap", "skip", "walk", "sing", "fight", "bash", "lead", "fall", "strike", "roar"),
-         "3rdsin":("calls", "runs", "is", "was", "eats", "drinks", "dies", "reads", "plays", "falls", "slap", "skips", "walk", "sing", "fight", "bash", "lead", "fall", "strike", "roar"),
-         "unconj":("to call", "to run", "to be", "to have been", "to eat", "to drink", "to die", "to read", "to play", "to fall", "to slap", "to skip", "to walk", "to sing", "to fight", "to bash", "to lead", "to fall", "to strike", "to roar")}
+VERBS = {"1stsin":("call", "run", "am", "was", "want", "eat", "drink", "die", "read", "play", "fall", "slap", "skip", "walk", "sing", "fight", "bash", "lead", "fall", "strike", "roar", "change", "speak", "talk", "preach"),
+         "2ndsin/3rdpl":("call", "run", "are", "were", "want", "eat", "drink", "die", "read", "play", "fall", "slap", "skip", "walk", "sing", "fight", "bash", "lead", "fall", "strike", "roar", "change", "speak", "talk", "preach"),
+         "3rdsin":("calls", "runs", "is", "was", "eats", "drinks", "dies", "reads", "plays", "falls", "slaps", "skips", "walks", "sings", "fights", "bashes", "leads", "falls", "strikes", "roars", "changes", "speaks", "talks", "preaches"),
+         "unconj":("to call", "to run", "to be", "to have been", "to eat", "to drink", "to die", "to read", "to play", "to fall", "to slap", "to skip", "to walk", "to sing", "to fight", "to bash", "to lead", "to fall", "to strike", "to roar", "to change", "to speak", "to talk", "to preach")}
 
 QW_CLAUSES = {"where":('"where {0} {1}".format(iFromList(NOUNS, "plural"), iFromList(VERBS, "2ndsin/3rdpl"))', '"where the {0} {1}".format(iFromList(NOUNS, "plural"), iFromList(VERBS, "2ndsin/3rdpl"))', '"where {0} and {1} {2}".format(iFromList(NOUNS, "plural"), iFromList(NOUNS, "plural"), iFromList(VERBS, "2ndsin/3rdpl"))', '"where the {0} {1}".format(iFromList(NOUNS, "singular"), iFromList(VERBS, "3rdsin"))'),
-              "who":('"who {0} the {1}".format(iFromList(VERBS, "3rdsin"), iFromList(NOUNS, "plural"))', '"who {0} {1}".format(iFromList(VERBS, "3rdsin"), iFromList(NOUNS, "plural"))', '"who {0} the {1}".format(iFromList(VERBS, "3rdsin"), iFromList(NOUNS, "singular"))', '"who {0} {1}".format(iFromList(VERBS, "3rdsin"), iFromList(NOUNS, "singular"))'), # famous people's names/titles
+              "who":('"who {0} the {1}".format(iFromList(VERBS, "3rdsin"), iFromList(NOUNS, "plural"))', '"who {0} {1}".format(iFromList(VERBS, "3rdsin"), iFromList(NOUNS, "plural"))', '"who {0} the {1}".format(iFromList(VERBS, "3rdsin"), iFromList(NOUNS, "singular"))', '"who {0} {1}".format(iFromList(VERBS, "3rdsin"), iFromList(NOUNS, "singular"))', '"who speaks with the {0}".format(iFromList(ANIMALS))'), # famous people's names/titles
               "when":('"when the {0} {1}".format(iFromList(NOUNS, "singular"), iFromList(VERBS, "3rdsin"))', '"when the {0} {1}".format(iFromList(NOUNS, "plural"), iFromList(VERBS, "2ndsin/3rdpl"))'),
-              "why":(), # why and how could both be for names of gods
-              "what":(),
-              "how":()}
+              "why":('"why the {0} {1}".format(iFromList(NOUNS, "plural"), iFromList(VERBS, "2ndsin/3rdpl"))', '"why the {0} {1}".format(iFromList(NOUNS, "singular"), iFromList(VERBS, "3rdsin"))', '"why the {0} {1}".format(iFromList(NOUNS, "singular"), iFromList(VERBS, "3rdsin"))'), # why and how could both be for names of gods
+              "what":('"what the {0} {1}".format(iFromList(NOUNS, "plural"), iFromList(VERBS, "2ndsin/3rdpl"))', '"what the {0} {1}".format(iFromList(NOUNS, "singular"), iFromList(VERBS, "3rdsin"))', '"what the {0} {1}".format(iFromList(NOUNS, "singular"), iFromList(VERBS, "3rdsin"))'),
+              "how":('"how the {0} {1}".format(iFromList(NOUNS, "plural"), iFromList(VERBS, "2ndsin/3rdpl"))', '"how the {0} {1}".format(iFromList(NOUNS, "singular"), iFromList(VERBS, "3rdsin"))', '"how the {0} {1}".format(iFromList(NOUNS, "singular"), iFromList(VERBS, "3rdsin"))')}
