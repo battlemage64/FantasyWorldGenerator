b8b1da441b8b9c59b8132990d62a5d6d1b7997ad
battlemage64 <arden@kolodner.com>
2018-09-23 21:21:42 -0400

    Added Voronoi point-centric point grouping to allow polygons to be handled directly. Not super inefficient, either, surprisingly! Removed the continent shape so it can be redone and randomized soon.

--- WorldGen/WorldGen 8th Shot -- Voronoi.py
+++ WorldGen/WorldGen 8th Shot -- Voronoi.py
@@ -10,7 +10,7 @@ random.seed(input("Enter a seed. \n>>> "))
 voronoi_points = [] # list of tuples (x, y, color): random points to draw polygons around
 
 def gen_color(): # generates a list of hex values to be turned into a color later with decrypt_color()
-    return [random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)]
+    return [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 
 def decrypt_color(data): # where data is a list of (r, g, b)
     color = "#"
@@ -25,19 +25,19 @@ def decrypt_color(data): # where data is a list of (r, g, b)
     return color
 
 for i in range(random.randint(50, 100)):
-    new_point = (random.randint(0, 499), random.randint(0, 499), gen_color())
-    if new_point[0] in range(0, 70) or new_point[0] in range(430, 500) or new_point[1] in range(0, 70) or new_point[1] in range(430, 500):
-        new_point[2][0] *= 0.2
-        new_point[2][1] *= 0.2
-    if new_point[0] in range(70, 100) or new_point[0] in range(400, 430) or new_point[1] in range(70, 100) or new_point[1] in range(400, 430):
-        new_point[2][1] *= 0.5
-        new_point[2][2] *= 0.8
-    if new_point[0] in range(101, 431) and new_point[1] in range(101, 431):
-        new_point[2][0] *= 0.2
-        new_point[2][2] *= 0.2
-        if new_point[0] in range(100, 400) and new_point[1] in range(100, 400) and random.randint(1, 10) == 1:
-            new_point[2][0] *= 10
-    voronoi_points.append(new_point) # generates random point and an assigned color
+    new_point = [random.randint(0, 499), random.randint(0, 499), gen_color(), []]
+##    if new_point[0] in range(0, 70) or new_point[0] in range(430, 500) or new_point[1] in range(0, 70) or new_point[1] in range(430, 500):
+##        new_point[2][0] *= 0.2
+##        new_point[2][1] *= 0.2
+##    if new_point[0] in range(70, 100) or new_point[0] in range(400, 430) or new_point[1] in range(70, 100) or new_point[1] in range(400, 430):
+##        new_point[2][1] *= 0.5
+##        new_point[2][2] *= 0.8
+##    if new_point[0] in range(101, 431) and new_point[1] in range(101, 431):
+##        new_point[2][0] *= 0.2
+##        new_point[2][2] *= 0.2
+##        if new_point[0] in range(100, 400) and new_point[1] in range(100, 400) and random.randint(1, 10) == 1:
+##            new_point[2][0] *= 10
+    voronoi_points.append(new_point) # generates random point and an assigned color and related points list
 
 window = tkinter.Tk()
 window.title("Your Finished Map")
@@ -48,16 +48,31 @@ canvas = tkinter.Canvas(master=window, width=500, height=500)
 canvas.pack()
 canvas.create_image(0, 0, image=canvasimage, anchor=tkinter.NW)
 
+##for i in range(0, 499):
+##    for j in range(0, 499):
+##        lowest = 999
+##        for point in voronoi_points:
+##            # The distance from the pixel to each point is calculated,
+##            # and then the pixel is turned the color of the closest
+##            # point. The formula below is to find shortest distance
+##            # between points: sqrt((x2-x1)^2+(y2-y1)^2)
+##            dist_to_point = math.sqrt((point[0]-i)**2+(point[1]-j)**2)
+##            if dist_to_point < lowest:
+##                lowest = dist_to_point
+##                colorSelected = point[2]
+##        canvasimage.put(decrypt_color(colorSelected), (i, j))
+
 for i in range(0, 499):
     for j in range(0, 499):
         lowest = 999
         for point in voronoi_points:
-            # The distance from the pixel to each point is calculated,
-            # and then the pixel is turned the color of the closest
-            # point. The formula below is to find shortest distance
-            # between points: sqrt((x2-x1)^2+(y2-y1)^2)
             dist_to_point = math.sqrt((point[0]-i)**2+(point[1]-j)**2)
             if dist_to_point < lowest:
                 lowest = dist_to_point
-                colorSelected = point[2]
-        canvasimage.put(decrypt_color(colorSelected), (i, j))
+                target = point
+        target[3].append((i, j))
+
+for point in voronoi_points:
+    for coords in point[3]:
+        canvasimage.put(decrypt_color(point[2]), coords)
+
