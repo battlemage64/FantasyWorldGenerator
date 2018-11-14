836eb897f22f2ac2a23238ab0c6c85c4c283d9ac
battlemage64 <arden@kolodner.com>
2018-11-13 21:39:36 -0500

    Finish test coastline generator by grid and perturbation, need to decide whether to check for lakes or create them

--- WorldGen/Test/WallFollower.py
+++ WorldGen/Test/WallFollower.py
@@ -19,7 +19,7 @@ for i in range(25):
 
 cells[random.randint(1, 23)][random.randint(1, 23)] = 1
 
-for a in range(10):
+for a in range(20):
    for i in range(2, 24):
       for j in range(2, 24):
          if cells[i][j] == 1 and random.randint(1, 3) != 1:
@@ -37,33 +37,41 @@ for a in range(10):
          if cells[i][j] == 2:
             cells[i][j] = 1
 
+def callback(event):
+    print("clicked at", int(event.x/20), int(event.y/20))
+    print(cells[int(event.x/20)][int(event.y/20)])
+
+window.bind("<Button-1>", callback)
+
+for i in range(25):
+    cells[i][0] = 0
+    cells[0][i] = 0
+    cells[i][24] = 0
+    cells[24][i] = 0
+
 outline_points = []
 outline_walls = {}
 
-for i in range(2, 24):
-    for j in range(2, 24):
+lakes = []
+
+for i in range(1, 24):
+    for j in range(1, 24):
         if cells[i][j]:
-            if not cells[i+1][j]: # nothing below
+            if not cells[i+1][j]: # nothing right
                 canvas.create_line((i+1)*20, j*20, (i+1)*20, (j+1)*20)
                 outline_walls[((i+1)*20, j*20)] = ((i+1)*20, (j+1)*20)
-            if not cells[i-1][j]: # nothing above
+            if not cells[i-1][j]: # nothing left
                 canvas.create_line(i*20, j*20, i*20, (j+1)*20)
-                outline_walls[(i*20, j*20)] = (i*20, (j+1)*20)
-            if not cells[i][j+1]: # nothing right
+                outline_walls[(i*20, (j+1)*20)] = (i*20, j*20)
+            if not cells[i][j+1]: # nothing below
                 canvas.create_line(i*20, (j+1)*20, (i+1)*20, (j+1)*20)
-                outline_walls[(i*20, (j+1)*20)] = ((i+1)*20, (j+1)*20)
-            if not cells[i][j-1]: # nothing left
+                outline_walls[((i+1)*20, (j+1)*20)] = (i*20, (j+1)*20)
+            if not cells[i][j-1]: # nothing above
                 canvas.create_line(i*20, j*20, (i+1)*20, j*20)
                 outline_walls[(i*20, j*20)] = ((i+1)*20, j*20)
-                
-##            if not cells[i+1][j] or not cells[i][j+1]:
-##                outline_points.append(((i+1)*20, (j+1)*20))
-##            if not cells[i-1][j] or not cells[i][j+1]:
-##                outline_points.append((i*20, (j+1)*20))
-##            if not cells[i-1][j] or not cells[i][j-1]:
-##                outline_points.append((i*20, j*20))
-##            if not cells[i+1][j] or not cells[i][j-1]:
-##                outline_points.append(((i+1)*20, j*20))
+
+        elif cells[i+1][j] and cells[i-1][j] and cells[i][j-1] and cells[i][j+1]: # is a lake
+            lakes.append((i, j))
 
 start = next(iter(outline_walls.keys()))
 endpoint = start
@@ -74,7 +82,16 @@ while True:
     if start == endpoint:
         break
 
-canvas.create_polygon(outline_points, fill='')
-    
+grid_polygon = canvas.create_polygon(outline_points, fill='', activefill='#00FF00')
 
-#canvas.create_polygon(outline_points)
+perturbed_points = []
+
+for point in outline_points:
+    perturbed_points.append((point[0] + random.randint(-15, 15), point[1] + random.randint(-15, 15)))
+
+perturbed_polygon = canvas.create_polygon(perturbed_points, fill='', outline='#FF0000')
+
+for point in lakes:
+    canvas.create_rectangle(point[0]*20, point[1]*20, (point[0]+1)*20, (point[1]+1)*20, fill='#000000')
+
+canvas.tag_raise(grid_polygon)
